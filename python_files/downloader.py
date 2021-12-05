e40d0d03722535ee66678b75f8b209e5f8fb7c87
kevinlowen <kevinlowen@gmail.com>
2021-12-05 15:17:25 -0500

    api queries are working

--- python_files/downloader.py
+++ python_files/downloader.py
@@ -1,8 +1,20 @@
-from nba_api.stats.endpoints import commonplayerinfo
+from nba_api.stats.endpoints import commonteamyears
+from nba_api.stats.endpoints import teamgamelog
+from nba_api.stats.library.parameters import Season
 
 
-def nba_scores_downloader():
-    return("test of nba_scores_downloader")
+def nba_teams_list():
+    teams = commonteamyears.CommonTeamYears(league_id='00')
+    df = teams.get_data_frames()[0]
+    print(df)
+
+def nba_team_games():
+    games = teamgamelog.TeamGameLog(season='1949-49', season_type_all_star='Regular Season', team_id='1610610037')
+    df = games.get_data_frames()[0]
+    print(df)
+
+
 
 if __name__ == "__main__":
-   nba_scores_downloader()
+   nba_teams_list()
+   nba_team_games()
