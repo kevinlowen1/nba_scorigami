e27debf78ffadf3b636f29737077178d9bf3fdd3
kevinlowen <kevinlowen@gmail.com>
2021-12-05 20:50:46 -0500

    passing dataframe to loop through season scores

--- python_files/downloader.py
+++ python_files/downloader.py
@@ -9,14 +9,18 @@ def nba_teams_list():
     teams = commonteamyears.CommonTeamYears(league_id='00')
     df = teams.get_data_frames()[0]
     print(df)
+    return(df)
 
-def nba_team_games():
-    games = teamgamelog.TeamGameLog(season='1949-49', season_type_all_star='Regular Season', team_id='1610610037')
-    df = games.get_data_frames()[0]
-    print(df)
+def nba_team_games(teams):
+    for row in teams.itertuples(index=False):
+        print(row[teams.columns.get_loc('TEAM_ID')], 
+                row[teams.columns.get_loc('MIN_YEAR')],
+                row[teams.columns.get_loc('MAX_YEAR')] )
 
-
+    # games = teamgamelog.TeamGameLog(season='1949-49', season_type_all_star='Regular Season', team_id='1610610037')
+    # df = games.get_data_frames()[0]
+    # print(df)
 
 if __name__ == "__main__":
-   nba_teams_list()
-   nba_team_games()
+   teams = nba_teams_list()
+   nba_team_games(teams)
