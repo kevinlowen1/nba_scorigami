7ff58382243daec9d1f3ceff9fb348cd0cd4ee1f
kevinlowen <kevinlowen@gmail.com>
2021-12-05 23:12:35 -0500

    for loop added to itterate through years of scores

--- python_files/downloader.py
+++ python_files/downloader.py
@@ -1,8 +1,9 @@
-from nba_api.stats.endpoints import commonteamyears
 ##https://github.com/swar/nba_api/blob/master/docs/nba_api/stats/endpoints/commonteamyears.md
 
+from nba_api.stats.endpoints import commonteamyears
 from nba_api.stats.endpoints import teamgamelog
-from nba_api.stats.library.parameters import Season
+# from nba_api.stats.library.parameters import Season
+import pandas as pd
 
 
 def nba_teams_list():
@@ -13,20 +14,36 @@ def nba_teams_list():
 
 def nba_team_games(teams):
     # print(teams[4].str.slice(start=2))
-    teams['MAX_YEAR'] = teams.MAX_YEAR.str.slice(start=-2)
-    print(teams)
-    # teams[3].apply(lambda x: x.str.slice(0, 3))
+    # teams['MAX_YEAR_ABBR'] = teams.MAX_YEAR.str.slice(start=-2)
+    # teams['MIN_YEAR_ABBR'] = teams.MIN_YEAR.str.slice(start=-2)
 
     for row in teams.itertuples(index=False):
-        print(row[teams.columns.get_loc('TEAM_ID')], 
-                row[teams.columns.get_loc('MIN_YEAR')],
-                row[teams.columns.get_loc('MAX_YEAR')]
-                 )
+        team = row[teams.columns.get_loc('TEAM_ID')]
+        min = row[teams.columns.get_loc('MIN_YEAR')]
+        max = row[teams.columns.get_loc('MAX_YEAR')]
 
-    # games = teamgamelog.TeamGameLog(season='1949-49', season_type_all_star='Regular Season', team_id='1610610037')
+        for x in range(min, max+1):
+            print(x)
+            games = teamgamelog.TeamGameLog(season=x, season_type_all_star='Regular Season', team_id=team)
+            df = games.get_data_frames()[0]
+            # print(df)
+
+        # print()
+        # print(row[teams.columns.get_loc('TEAM_ID')], 
+        #         row[teams.columns.get_loc('MIN_YEAR')],
+        #         row[teams.columns.get_loc('MAX_YEAR')],
+        #         row[teams.columns.get_loc('MIN_YEAR_ABBR')],
+        #         row[teams.columns.get_loc('MAX_YEAR_ABBR')]
+        #          )
+
+    # games = teamgamelog.TeamGameLog(season='2021', season_type_all_star='Regular Season', team_id='1610612737')
     # df = games.get_data_frames()[0]
     # print(df)
 
 if __name__ == "__main__":
-   teams = nba_teams_list()
-   nba_team_games(teams)
+#    teams = nba_teams_list()
+
+    data = [['00','1610612737',2016,2017,'ATL'] , ['00','1610612738',2017,2019,'BOS']]
+    teams = pd.DataFrame(data, columns = ['LEAGUE_ID','TEAM_ID','MIN_YEAR','MAX_YEAR','ABBREVIATION'])
+
+    nba_team_games(teams)
