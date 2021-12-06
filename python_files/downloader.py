8209501420284bacb0302786832cbeb89dc8a9f6
kevinlowen <kevinlowen@gmail.com>
2021-12-05 21:23:35 -0500

    Max_year abbreviated

--- python_files/downloader.py
+++ python_files/downloader.py
@@ -12,10 +12,16 @@ def nba_teams_list():
     return(df)
 
 def nba_team_games(teams):
+    # print(teams[4].str.slice(start=2))
+    teams['MAX_YEAR'] = teams.MAX_YEAR.str.slice(start=-2)
+    print(teams)
+    # teams[3].apply(lambda x: x.str.slice(0, 3))
+
     for row in teams.itertuples(index=False):
         print(row[teams.columns.get_loc('TEAM_ID')], 
                 row[teams.columns.get_loc('MIN_YEAR')],
-                row[teams.columns.get_loc('MAX_YEAR')] )
+                row[teams.columns.get_loc('MAX_YEAR')]
+                 )
 
     # games = teamgamelog.TeamGameLog(season='1949-49', season_type_all_star='Regular Season', team_id='1610610037')
     # df = games.get_data_frames()[0]
