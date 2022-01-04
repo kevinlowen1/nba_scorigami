a9823003c4f6adc7c3f75e8cc88b643c6e7b3775
kevinlowen <kevinlowen@gmail.com>
2022-01-03 21:03:33 -0500

    modifying to loop through all teams game logs

--- python_files/mysql_uploader.py
+++ python_files/mysql_uploader.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import csv
 import mariadb
+import os
 
 def mysql_table_creator_scores():
     #logging and printing
@@ -74,20 +75,21 @@ def mysql_table_creator_scores():
     f.write("created table for distinct games.\n")
     f.close()
 
-def mysql_inserter():
+def mysql_inserter(file):
     #logging and printing
     f = open("C:\\Users\\Kevin.DESKTOP-9D0VMK8\Documents\\Projects\\nba_scorigami\\logs\\log.txt", "a")
     f.write("starting mysql_inserter at: " + str(datetime.now()) + "\n")
-    print("starting mysql_inserter")
+    print("starting mysql_inserter for:" + file)
 
-    game_log_pandas = pd.read_csv('C:\\Users\\Kevin.DESKTOP-9D0VMK8\Documents\\Projects\\nba_scorigami\\game_scores\\1610610023_teamGameLog.csv', sep=',', quotechar='"')
+    game_log_pandas = pd.read_csv('C:\\Users\\Kevin.DESKTOP-9D0VMK8\Documents\\Projects\\nba_scorigami\\game_scores\\'+file, sep=',', quotechar='"')
+    # game_log_pandas = pd.read_csv('C:\\Users\\Kevin.DESKTOP-9D0VMK8\Documents\\Projects\\nba_scorigami\\game_scores\\1610610023_teamGameLog.csv', sep=',', quotechar='"')
     game_log_pandas = game_log_pandas.fillna(0)
     game_log_pandas = game_log_pandas.iloc[: , 1:]
     cols = ['Team_ID','Game_ID','W','L','MIN','FGM','FG3A','FGA','FG3M','FTM','FTA','OREB','DREB','REB','AST','STL','BLK','TOV','PF','PTS']
     game_log_pandas[cols] = game_log_pandas[cols].applymap(np.int64)
 
-    print("here")
-    print(game_log_pandas.columns[(game_log_pandas == 0.0).iloc[0]])
+    # print("here")
+    # print(game_log_pandas.columns[(game_log_pandas == 0.0).iloc[0]])
 
     conn = mariadb.connect(
         host=config.db_ip,
@@ -98,17 +100,18 @@ def mysql_inserter():
     )
     mycursor = conn.cursor()
 
-
     # Here, change your own path to dump the temp file
-    game_log_pandas.to_csv('C:\\Users\\Kevin.DESKTOP-9D0VMK8\Documents\\Projects\\nba_scorigami\\game_scores_no_index\\1610610023_teamGameLog.csv', index=False)                                                       
-    file = open('C:\\Users\\Kevin.DESKTOP-9D0VMK8\Documents\\Projects\\nba_scorigami\\game_scores_no_index\\1610610023_teamGameLog.csv','r')
-    csv_data = csv.reader(file)
+    game_log_pandas.to_csv('C:\\Users\\Kevin.DESKTOP-9D0VMK8\Documents\\Projects\\nba_scorigami\\game_scores_no_index\\'+file, index=False) 
+    # game_log_pandas.to_csv('C:\\Users\\Kevin.DESKTOP-9D0VMK8\Documents\\Projects\\nba_scorigami\\game_scores_no_index\\1610610023_teamGameLog.csv', index=False)                                                       
+    # file = open('C:\\Users\\Kevin.DESKTOP-9D0VMK8\Documents\\Projects\\nba_scorigami\\game_scores_no_index\\1610610023_teamGameLog.csv','r')                                                 
+    filed = open('C:\\Users\\Kevin.DESKTOP-9D0VMK8\Documents\\Projects\\nba_scorigami\\game_scores_no_index\\'+file,'r')
+    csv_data = csv.reader(filed)
 
     my_data = []
     for row in csv_data:
         my_data.append(row)
 
-    with open('C:\\Users\\Kevin.DESKTOP-9D0VMK8\Documents\\Projects\\nba_scorigami\\game_scores_no_index\\1610610023_teamGameLog.csv','r', newline='') as f:
+    with open('C:\\Users\\Kevin.DESKTOP-9D0VMK8\Documents\\Projects\\nba_scorigami\\game_scores_no_index\\'+file,'r', newline='') as f:
         next(f)
         reader = csv.reader(f, delimiter=',')
         rows = [[int(row[0]),int(row[1]),row[2],row[3],row[4],int(row[5]),int(row[6]),float(row[7]),int(row[8]),int(row[9]),int(row[10]),float(row[11]),int(row[12]),int(row[13]),float(row[14]),int(row[15]),int(row[16]),float(row[17]),int(row[18]),int(row[19]),int(row[20]),int(row[21]),int(row[22]),int(row[23]),int(row[24]),int(row[25]),int(row[26])] for row in reader if row]
@@ -131,7 +134,11 @@ if __name__ == "__main__":
     f.close()
 
     mysql_table_creator_scores()
-    mysql_inserter()
+
+    directory = r'C:\\Users\\Kevin.DESKTOP-9D0VMK8\\Documents\\Projects\\nba_scorigami\\game_scores'
+
+    for file in os.listdir(directory):
+        mysql_inserter(file)
 
 
 
